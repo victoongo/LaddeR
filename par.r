ec70030717fd2787114b76f094f294b6148da8f0
Victor Wang <victoongo@gmail.com>
2014-10-30 00:33:35 -0400

    test doParallel sample code

---
+++ par.r
@@ -0,0 +1,29 @@
+library(doParallel)
+cl <- makeCluster(2)
+registerDoParallel(cl)
+foreach(i=1:3) %dopar% {
+  d <- sqrt(i)
+  d
+}
+
+
+x <- iris[which(iris[,5] != "setosa"), c(1,5)]
+trials <- 10000
+
+ptime <- system.time({
+  r <- foreach(icount(trials), .combine=cbind) %dopar% {
+    ind <- sample(100, 100, replace=TRUE)
+    result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+    coefficients(result1)
+  }
+})[3]
+ptime
+
+stime <- system.time({
+  r <- foreach(icount(trials), .combine=cbind) %do% {
+    ind <- sample(100, 100, replace=TRUE)
+    result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+    coefficients(result1)
+  }
+})[3]
+stime
