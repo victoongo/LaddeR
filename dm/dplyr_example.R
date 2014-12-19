4770d0b4b992546d9ab9a8cdb323abccdb0c67c0
Victor Wang <victoongo@gmail.com>
2014-12-19 13:02:57 -0500

    new graph added

--- dm/dplyr_example.R
+++ dm/dplyr_example.R
@@ -5,6 +5,21 @@ library(datasets)
 data(package = .packages(all.available=TRUE))
 data(diamonds)
 head(diamonds)
+
+outcome <- list('price')
+outcome[1]
+for (var in outcome) {
+  print(var)
+x  test <- qplot(carat, diamonds[,var], data=diamonds)
+  test
+  summary(test)
+}
+test
+print(test)
+outcome <- "price"
+test <- qplot(carat, outcome, data=diamonds)
+summary(test)
+
 qplot(carat, price, data=diamonds)
 qplot(log(carat), log(price), data=diamonds)
 qplot(carat, price/carat, data=diamonds)
@@ -17,8 +32,19 @@ qplot(price, carat, data=diamonds, facets=color ~ clarity)
 
 qplot(cut, data=diamonds, geom="bar")
 qplot(price, data=diamonds, geom="histogram")
+qplot(price, data=diamonds, geom="histogram", color=color)
+qplot(price, data=diamonds, geom="histogram", fill=color)
 qplot(price, data=diamonds, geom="histogram", binwidth=100) # # of bins
 qplot(price, data=diamonds, geom="histogram", xlim=c(0, 5000)) # zooming
+
+qplot(carat, data=diamonds, geom="density", colour=color)
+qplot(carat, data=diamonds, geom="histogram", fill=color, binwidth=0.5)
+
+qplot(color, price/carat, data=diamonds, geom="boxplot", colour=color)
+qplot(color, price/carat, data=diamonds, geom="boxplot", fill=color)
+
+qplot(color, price / carat, data=diamonds, geom="jitter", colour=color, alpha=I(1/8))
+
 
 df.diamonds_ideal <- filter(diamonds, cut=="Ideal") # select rows
 df.diamonds_ideal <- select(df.diamonds_ideal, carat, cut, color, price, clarity) # select vars
