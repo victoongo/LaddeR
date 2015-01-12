9f5fff0f004a0376c6b62b82ae1f35146b58f52f
Victor Wang <victoongo@gmail.com>
2015-01-12 09:57:49 -0500

    new parallel program

--- par.r
+++ par.r
@@ -7,12 +7,29 @@ foreach(i=1:3) %dopar% {
   d <- sqrt(i)
   d
 }
+
 names(iris)
 y_var <- c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")
-foreach(y=y_var, .verbose=TRUE, .combine=cbind) %do% {
-  fmla <- paste(y, "~factor(Species)")
-  result <- lm(as.formula(fmla), data=iris)
-}
+for (y in y_var) {y <- 1}
+# ptime <- system.time({
+# r <- foreach(y=y_var, .verbose=TRUE, .combine=rbind) %dopar% {
+#     foreach(y=y_var, .verbose=TRUE, .combine=rbind) %dopar% {
+      for(y in y_var) {
+#     fmla <- paste(y, "~factor(Species)")
+#     result <- data.frame(coef(summary(lm(as.formula(fmla), data=iris))))
+#     fmla <- as.formula(paste(y, "~factor(Species)"))
+#     fmla <- paste(y, "~factor(Species)")
+#     result <- data.frame(coef(summary(lm(fmla, data=iris))))
+#     result$outcome <- y
+#     return(result)
+    fmla <- paste(y, "~factor(Species)")
+#     out <- paste(y)
+#     y <- data.frame(coef(summary(lm(fmla, data=iris))))
+    y[] <- data.frame(coef(summary(lm(fmla, data=iris))))
+  }
+# })[3]
+ptime
+
 tb <- data.frame(coef(summary(result)))
 # summary(lm(Sepal.Length~factor(Species), data=iris))
 
